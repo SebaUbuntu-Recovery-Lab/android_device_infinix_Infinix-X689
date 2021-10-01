d270ec5c0b6a3017f960db1f4f9d87070f269c69
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-01 09:42:11 +0200

    Infinix-X689: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: infinix Model: Infinix X689 Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X689.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X689-user \
+    omni_Infinix-X689-userdebug \
+    omni_Infinix-X689-eng
